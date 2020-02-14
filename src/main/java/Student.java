390e658d09c4d8840dee6be93a79ed463712e75b
Noland Seigler <nolandseigler@gmail.com>
2020-02-14 14:43:25 -0600

    part one complete

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 public class Student {
@@ -8,25 +9,35 @@ public class Student {
 
 
     Student(String name) {
-
+        this.name = name;
+        counter++;
+        this.id = counter;
+        this.grades = new ArrayList<>();
     }
     // returns the student's id
     public long getId() {
-        return 0;
+        return this.id;
     }
 
     // returns the student's name
     public String getName() {
-        return null;
+        return this.name;
     }
 
     // adds the given grade to the grades list
     public void addGrade(int grade) {
-
+        this.grades.add(grade);
     }
-
+    //ADDED THIS GETTER TO ACCESS GRADES FOR TESTING
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
     // returns the average of the students grades
     public double getGradeAverage() {
-        return 0;
+        double sum = 0;
+        for(Integer grade : this.grades) {
+            sum += grade;
+        }
+        return sum / this.grades.size();
     }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,5 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class StudentTest {
@@ -17,18 +20,36 @@ public class StudentTest {
 //    // returns the student's id
 //    public long getId();
     @Test
-    public void validId() {
+    public void testGetId() {
         long expected = 0L;
         long actual = withoutGrades.getId();
-        assertTrue("Student ID is greater than zero.", actual > expected);
+        assertTrue("Student ID is must be greater than zero.", actual > expected);
         expected = actual;
         actual = withGrades.getId();
         assertTrue("IDs should increase with as students are added.", actual > expected);
     }
 //    // returns the student's name
 //    public String getName();
+    @Test
+    public void testGetName() {
+        String expected = "Bob";
+        String actual = withoutGrades.getName();
+        assertEquals("getName() should return the String name instance field.", expected, actual);
+    }
 //    // adds the given grade to the grades list
 //    public void addGrade(int grade);
+    @Test
+    public void testAddGrade() {
+        ArrayList<Integer> blankGrades = withoutGrades.getGrades();
+        ArrayList<Integer> hasGrades = withGrades.getGrades();
+        assertTrue("Adding grades should increase the List size.", hasGrades.size() > blankGrades.size());
+    }
 //    // returns the average of the students grades
 //    public double getGradeAverage();
+    @Test
+    public void testGetAverage() {
+        double expected = (90 + 80 + 100) / 3;
+        double actual = withGrades.getGradeAverage();
+        assertEquals(expected, actual, 0);
+    }
 }
