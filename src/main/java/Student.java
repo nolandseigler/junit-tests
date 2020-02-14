62fe500a4e5a4e1e95ce3c97fc76d6a88a0afc6a
Noland Seigler <nolandseigler@gmail.com>
2020-02-14 12:33:28 -0600

    developing tests

---
+++ src/main/java/Student.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private long id;
+    private static long counter;
+    private ArrayList<Integer> grades;
+
+
+    Student(String name) {
+
+    }
+    // returns the student's id
+    public long getId() {
+        return 0;
+    }
+
+    // returns the student's name
+    public String getName() {
+        return null;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade) {
+
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        return 0;
+    }
+}
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    Student withoutGrades;
+    Student withGrades;
+
+    @Before
+    public void setUp() {
+        withoutGrades = new Student("Bob");
+        withGrades = new Student("Suzie");
+        withGrades.addGrade(90);
+        withGrades.addGrade(80);
+        withGrades.addGrade(100);
+    }
+//    // returns the student's id
+//    public long getId();
+    @Test
+    public void validId() {
+        long expected = 0L;
+        long actual = withoutGrades.getId();
+        assertTrue("Student ID is greater than zero.", actual > expected);
+        expected = actual;
+        actual = withGrades.getId();
+        assertTrue("IDs should increase with as students are added.", actual > expected);
+    }
+//    // returns the student's name
+//    public String getName();
+//    // adds the given grade to the grades list
+//    public void addGrade(int grade);
+//    // returns the average of the students grades
+//    public double getGradeAverage();
 }
